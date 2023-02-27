c73de02aabf6bd1b784c3bbc4b102319dd94a407
jaimeload <jaime.si.load@gmail.com>
2023-02-27 19:09:13 +0530

    HW_3.1

--- HW_3/18.py
+++ HW_3/18.py
@@ -1,10 +1,7 @@
-# Как и эта
-
 from random import randint
 n = int(input("Введите количество элементов: "))
 x = int(input("Введите число: "))
 lst = [randint(1, 7) for i in range(n)]
-for i in lst:
-    nearest = min(lst, key=lambda i:abs(i - x))
+nearest = min(lst, key=lambda i:abs(i - x))
 print(lst)
 print(f'{nearest} ближайшее число к {x}')
