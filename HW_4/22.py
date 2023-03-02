519a1ba8a9a939a722fba441892cbb015a776dd6
jaimeload <118521499+jaimeload@users.noreply.github.com>
2023-03-02 13:24:23 +0530

    Update 22.py

--- HW_4/22.py
+++ HW_4/22.py
@@ -3,7 +3,6 @@ n = int(input("Введите количество элементов перво
 m = int(input("Введите количество элементов второго набора чисел: "))
 lst1 = [randint(1, 20) for i in range(n)]
 lst2 = [randint(1, 20) for i in range(m)]
-lst3 = set()
 print(lst1)
 print(lst2)
 set3 = set([i for i in lst1 for j in lst2 if i in lst2])
